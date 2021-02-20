23a461ac4fe31ec7d84094db8254f6f224edb610
pagmerek <p.gmerek@student.uj.edu.pl>
2021-02-20 16:05:27 +0100

    add readme.md and handleConnection func

--- main.go
+++ main.go
@@ -11,7 +11,46 @@ import (
 
 var words = make(map[string]string)
 
-func main() {
+func handleConnection(connection net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("New connection")
+	for {
+		reader := bufio.NewReader(connection)
+		for {
+			reply, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println("Connection closed")
+				break
+			}
+			reply += " "
+			request := strings.Split(reply, " ")
+			switch request[0] {
+			case "GET":
+				anwser, ok := words[request[1]]
+				if ok {
+					connection.Write([]byte("ANWSER " + anwser + "\n"))
+				} else {
+					connection.Write([]byte("ERROR can't find " + request[1] + "\n"))
+				}
+			case "SET":
+				words[request[1]] = strings.Join(request[2:], " ")
+				connection.Write([]byte("ADD DEFINITION FOR " + request[1] + "\n"))
+			case "CLEAR":
+				words = make(map[string]string)
+			case "ALL":
+				for k, v := range words {
+					connection.Write([]byte("WORD: " + k + " || DEFINITION: " + v + "\n"))
+				}
+			}
+		}
+
+	}
+}
+func main() { 
+	
+"""
+Main function that runs the sever and runs goroutine that accepts connections
+"""
 	fmt.Println("Start server ...")
 	socket, _ := net.Listen("tcp", ":8000")
 	wg := new(sync.WaitGroup)
@@ -23,40 +62,7 @@ func main() {
 				return
 			}
 			wg.Add(1)
-			go func(connection net.Conn) {
-				defer wg.Done()
-				for {
-					reader := bufio.NewReader(connection)
-					for {
-						reply, err := reader.ReadString('\n')
-						if err != nil {
-							fmt.Println("Connection closed")
-							break
-						}
-						reply += " "
-						request := strings.Split(reply, " ")
-						switch request[0] {
-						case "GET":
-							anwser, ok := words[request[1]]
-							if ok {
-								connection.Write([]byte("ANWSER " + anwser + "\n"))
-							} else {
-								connection.Write([]byte("ERROR can't find " + request[1] + "\n"))
-							}
-						case "SET":
-							words[request[1]] = strings.Join(request[2:], " ")
-							connection.Write([]byte("ADD DEFINITION FOR " + request[1] + "\n"))
-						case "CLEAR":
-							words = make(map[string]string)
-						case "ALL":
-							for k, v := range words {
-								connection.Write([]byte("WORD: " + k + " || DEFINITION: " + v + "\n"))
-							}
-						}
-					}
-
-				}
-			}(c)
+			go handleConnection(c, wg)
 		}
 	}(socket)
 	time.Sleep(1 * time.Minute)
---
+++ readme.md
@@ -0,0 +1,7 @@
+# simple go http server
+Go http server
+## Basic Usage
+To run it just type in the terminal
+` go run main.go `
+To connect to the server use
+`nc -v -v localhost 8000`
