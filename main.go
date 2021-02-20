e38419a0f6c0e5aa8ae8e58d0e5be090ffcbaa65
pagmerek <p.gmerek@student.uj.edu.pl>
2021-02-20 14:57:34 +0100

    add goroutines to support multiple clients

--- main.go
+++ main.go
@@ -5,41 +5,60 @@ import (
 	"fmt"
 	"net"
 	"strings"
+	"sync"
+	"time"
 )
 
+var words = make(map[string]string)
+
 func main() {
-	words := make(map[string]string)
-
 	fmt.Println("Start server ...")
-
 	socket, _ := net.Listen("tcp", ":8000")
-	connection, _ := socket.Accept()
-	reader := bufio.NewReader(connection)
-	for {
-		reply, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println("Connection closed")
-			break
-		}
-		reply += " "
-		request := strings.Split(reply, " ")
-		switch request[0] {
-		case "GET":
-			anwser, ok := words[request[1]]
-			if ok {
-				connection.Write([]byte("ANWSER " + anwser + "\n"))
-			} else {
-				connection.Write([]byte("ERROR can't find " + request[1] + "\n"))
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	go func(l net.Listener) {
+		for {
+			c, err := l.Accept()
+			if err != nil {
+				return
 			}
-		case "SET":
-			words[request[1]] = strings.Join(request[2:], " ")
-			connection.Write([]byte("ADD DEFINITION FOR " + request[1] + "\n"))
-		case "CLEAR":
-			words = make(map[string]string)
-		case "ALL":
-			for k, v := range words {
-				connection.Write([]byte("WORD: " + k + " || DEFINITION: " + v + "\n"))
-			}
+			wg.Add(1)
+			go func(connection net.Conn) {
+				defer wg.Done()
+				for {
+					reader := bufio.NewReader(connection)
+					for {
+						reply, err := reader.ReadString('\n')
+						if err != nil {
+							fmt.Println("Connection closed")
+							break
+						}
+						reply += " "
+						request := strings.Split(reply, " ")
+						switch request[0] {
+						case "GET":
+							anwser, ok := words[request[1]]
+							if ok {
+								connection.Write([]byte("ANWSER " + anwser + "\n"))
+							} else {
+								connection.Write([]byte("ERROR can't find " + request[1] + "\n"))
+							}
+						case "SET":
+							words[request[1]] = strings.Join(request[2:], " ")
+							connection.Write([]byte("ADD DEFINITION FOR " + request[1] + "\n"))
+						case "CLEAR":
+							words = make(map[string]string)
+						case "ALL":
+							for k, v := range words {
+								connection.Write([]byte("WORD: " + k + " || DEFINITION: " + v + "\n"))
+							}
+						}
+					}
+
+				}
+			}(c)
 		}
-	}
+	}(socket)
+	time.Sleep(1 * time.Minute)
+	wg.Wait()
 }
